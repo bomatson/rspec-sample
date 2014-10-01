5d0c8dbebedcf877791033003e7891913ecf8e19
bomatson <bobby@carbonfive.com>
2014-09-30 22:08:49 -0700

    initialize dragon and give it a name

--- dragon.rb
+++ dragon.rb
@@ -1,3 +1,17 @@
 class Dragon
+  attr_accessor :name, :magical, :scales
 
+  def initialize(opts = {})
+    @magical = opts.fetch(:magical, false)
+  end
+
+  def fly
+    'flying'
+  end
+
+  # can fly
+  # has a name
+  # can only breathe fire if its magical
+  # has a scale count
+  # can hatch eggs
 end
--- spec/dragon_spec.rb
+++ spec/dragon_spec.rb
@@ -2,7 +2,48 @@ require_relative '../dragon.rb'
 require 'rspec'
 
 describe Dragon do
+
   it 'exists' do
     expect(Dragon).to be
+  end
+
+  # notice the let statements
+
+  describe 'on initialize' do
+    context 'without magic' do
+      let(:dragon) { Dragon.new }
+
+      it 'is not magical' do
+        expect(dragon.magical).to eq false
+      end
+    end
+
+    context 'with magic' do
+      let(:dragon) { Dragon.new(magical: true) }
+
+      it 'is totally magical' do
+        expect(dragon.magical).to eq true
+      end
+    end
+  end
+
+  # notice the subject usage
+
+  describe '#name' do
+    subject { Dragon.new }
+
+    before do
+      subject.name = 'roger'
+    end
+
+    it 'can be set' do
+      expect(subject.name).to eq 'roger'
+    end
+  end
+
+  describe '#fly' do
+    it 'tells us the dragon is flying' do
+      expect(subject.fly).to eq 'flying'
+    end
   end
 end
