55a199f1457c4c7d5f81b1d11c24c50b69af5b1b
bomatson <bobby@carbonfive.com>
2014-09-30 22:52:06 -0700

    include a few more examples

--- dragon.rb
+++ dragon.rb
@@ -1,5 +1,5 @@
 class Dragon
-  attr_accessor :name, :magical, :scales
+  attr_accessor :name, :magical, :age
 
   def initialize(opts = {})
     @magical = opts.fetch(:magical, false)
@@ -9,9 +9,38 @@ class Dragon
     'flying'
   end
 
+  def breathe_fire
+    if @magical
+      'screaming villagers'
+    else
+      'air'
+    end
+  end
+
+  def lay_eggs
+    if age > 21
+      "just layed #{egg_count} eggs!"
+    else
+      time_left = (21 - @age)
+      "you still have #{time_left} years"
+    end
+  end
+
+  def hatch!
+    if egg_count
+      egg_count.times do
+        Dragon.new
+      end
+    end
+  end
+
+  def egg_count
+    @egg_count ||= rand(10)
+  end
+
   # can fly
   # has a name
   # can only breathe fire if its magical
-  # has a scale count
+  # has an age
   # can hatch eggs
 end
--- spec/dragon_spec.rb
+++ spec/dragon_spec.rb
@@ -27,7 +27,7 @@ describe Dragon do
     end
   end
 
-  # notice the subject usage
+  # notice the subject usage & before
 
   describe '#name' do
     subject { Dragon.new }
@@ -41,9 +41,88 @@ describe Dragon do
     end
   end
 
+  describe '#age' do
+    subject { Dragon.new }
+
+    before do
+      subject.age = 10
+    end
+
+    it 'can be set' do
+      expect(subject.age).to eq 10
+    end
+  end
+
   describe '#fly' do
     it 'tells us the dragon is flying' do
       expect(subject.fly).to eq 'flying'
+    end
+  end
+
+  # notice the let setting context for conditionals
+
+  describe '#breathe_fire' do
+    context 'given a magical dragon' do
+      let(:dragon) { Dragon.new(magical: true) }
+
+      it 'breathes fire all over the place' do
+        expect(dragon.breathe_fire).to eq 'screaming villagers'
+      end
+    end
+
+    context 'given a non-magical dragon' do
+      let(:dragon) { Dragon.new }
+
+      it 'breathes fire all over the place' do
+        expect(dragon.breathe_fire).to eq 'air'
+      end
+    end
+  end
+
+  # notice use of stubs with a return value
+
+  describe '#lay_eggs' do
+    context 'given a of-age dragon' do
+      let(:dragon) { Dragon.new }
+
+      before do
+        dragon.age = 22
+        expect(dragon).to receive(:egg_count) { 5 }
+      end
+
+      it 'lays eggs' do
+        expect(dragon.lay_eggs).to eq "just layed 5 eggs!"
+      end
+    end
+
+    context 'given a under-age dragon' do
+      let(:dragon) { Dragon.new }
+
+      before do
+        dragon.age = 4
+      end
+
+      it 'tells the dragon how long it has' do
+        expect(dragon.lay_eggs).to eq 'you still have 17 years'
+      end
+    end
+  end
+
+  # notice use of mock expectations of Dragon
+
+  describe '#hatch' do
+    context 'given an egg_count' do
+      let(:dragon) { Dragon.new }
+
+      before do
+        expect(dragon).to receive(:egg_count) { 5 }
+          .at_least(:once)
+      end
+
+      it 'creates more dragons' do
+        expect(Dragon).to receive(:new).exactly(5).times
+        dragon.hatch!
+      end
     end
   end
 end
